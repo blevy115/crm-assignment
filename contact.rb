03c312e693427b152714445bed949811f1df2b10
Brandon Levy <brandon.levy@mail.mcgill.ca>
2017-11-08 12:55:23 -0500

    added first few methods

--- contact.rb
+++ contact.rb
@@ -1,28 +1,41 @@
 class Contact
 
+@@all_contacts = []
+@@id = 1
   # This method should initialize the contact's attributes
-  def initialize
-
+  def initialize(first_name, last_name, email, note)
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @note = note
+    @id = @@id
+    @@id += 1
   end
 
-  # This method should call the initializer, 
+  # This method should call the initializer,
   # store the newly created contact, and then return it
-  def self.create
-
+  def self.create(first_name, last_name, email, note)
+    new_contact = Contact.new(first_name, last_name, email, note)
+    @@all_contacts << new_contact
+    return new_contact
   end
 
   # This method should return all of the existing contacts
   def self.all
-
+     @@all_contacts
   end
 
   # This method should accept an id as an argument
   # and return the contact who has that id
-  def self.find
-
+  def self.find(id)
+    @@all_contacts.each do |contact|
+      if contact.id == id
+        return contact
+      end
+    end
   end
 
-  # This method should allow you to specify 
+  # This method should allow you to specify
   # 1. which of the contact's attributes you want to update
   # 2. the new value for that attribute
   # and then make the appropriate change to the contact
@@ -53,6 +66,41 @@ class Contact
 
   end
 
+  def first_name
+    @first_name
+  end
+
+  def first_name=(first_name)
+    @first_name = first_name
+  end
+
+  def last_name
+    @last_name
+  end
+
+  def last_name=(last_name)
+    @last_name = last_name
+  end
+
+  def email
+    @email
+  end
+
+  def email=(email)
+    @email = email
+  end
+
+  def note
+    @note
+  end
+
+  def note=(note)
+    @note = note
+  end
+
+  def id
+    @id
+  end
   # Feel free to add other methods here, if you need them.
-  
+
 end
